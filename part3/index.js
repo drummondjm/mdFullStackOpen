683725e612b5985bcfe5d04ef5ded4a386013a0d
Mark Drummond <mark.drummond@version1.com>
2024-09-11 17:27:41 +0100

    Part 3

--- part3/index.js
+++ part3/index.js
@@ -80,5 +80,5 @@ app.delete('/api/notes/:id', (request, response) => {
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server runningg on port ${PORT}`)
 })
